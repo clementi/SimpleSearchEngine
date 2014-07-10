5126d1e5b68c80756a5a980d54835264b2d05a06
Jeff Pratt <user@>
2014-07-09 19:48:30 -0700

    Add second test

--- SimpleSearchEngine.Indexing.Tests/IndexTest.cs
+++ SimpleSearchEngine.Indexing.Tests/IndexTest.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Machine.Specifications;
 
 namespace SimpleSearchEngine.Indexing.Tests
 {
     // ReSharper disable InconsistentNaming
     // ReSharper disable UnusedMember.Local
-    public class when_getting_a_word_that_is_in_the_index
+    [Subject("IIndex")]
+    class when_getting_a_word_that_is_in_the_index
     {
         static IIndex index;
         static readonly ISet<Guid> expectedOccurrences = new HashSet<Guid>
@@ -24,6 +26,19 @@ namespace SimpleSearchEngine.Indexing.Tests
         Because of = () => actualOccurrences = index.Get("word");
 
         It should_get_the_occurrences_of_that_word = () => actualOccurrences.SetEquals(expectedOccurrences);
+    }
+
+    [Subject("IIndex")]
+    class when_getting_a_word_that_is_not_in_the_index
+    {
+        static IIndex index;
+        static ISet<Guid> actualOccurrences; 
+
+        Establish context = () => index = new Index();
+
+        Because of = () => actualOccurrences = index.Get("word");
+
+        It should_get_an_empty_set = () => actualOccurrences.Any().ShouldBeFalse();
     }
     // ReSharper restore UnusedMember.Local
     // ReSharper restore InconsistentNaming
